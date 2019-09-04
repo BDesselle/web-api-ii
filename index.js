ad37d50e5e3b259bd6c4cafd8fa84649809ffff2
Brandon Desselle <desselle.brandon2@gmail.com>
2019-09-04 17:12:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const postsRoutes = require("./routes/postsRoutes");
+const postsRoutes = require("./routes/postRoute");
 
 const port = 6000;
 const server = express();
 
 server.use(express.json());
 
-server.use("/api", postsRoutes);
+server.use("/api/posts", postsRoutes);
 
 //* Listening Message
 server.listen(port, () => {
